4690dd31c561976ebae7d46cb71206ccb01153cd
Jeff Swenson <jeffongit@gmail.com>
2016-08-24 14:14:03 -0600

    Testing something

--- LoadTestEmTests/HATests.cs
+++ LoadTestEmTests/HATests.cs
@@ -13,184 +13,184 @@ namespace LoadTestEm.Tests
     {
         private string conStr = "Data Source=u7ksu7yxs0.database.windows.net;Database=SharedEpisodeTracker;Integrated Security=False;User ID=ETDev;Password=d3v3lupm!nt;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
-        [TestMethod()]
-        public void TestEmTest1()
-        {
-            var agent = new SqlLoadTask
-            {
-                ConnectionString = conStr,
-                Command = "select * from DataPointView where ProfileID = @profileId"
-            };
+        //[TestMethod()]
+        //public void TestEmTest1()
+        //{
+        //    var agent = new SqlLoadTask
+        //    {
+        //        ConnectionString = conStr,
+        //        Command = "select * from DataPointView where ProfileID = @profileId"
+        //    };
 
-            agent.CommandParameters.Add(new KeyValuePair<string, IValueGetter>("@profileId", new StringValueGetter("170FD672-7B0B-4A97-B0A7-6B2FADD148D4")));
+        //    agent.CommandParameters.Add(new KeyValuePair<string, IValueGetter>("@profileId", new StringValueGetter("170FD672-7B0B-4A97-B0A7-6B2FADD148D4")));
 
-            var viewResult = agent.Execute();
-            Trace.WriteLine($"1");
-            Trace.WriteLine($"Results from  view {viewResult.ExecutionTime}");
+        //    var viewResult = agent.Execute();
+        //    Trace.WriteLine($"1");
+        //    Trace.WriteLine($"Results from  view {viewResult.ExecutionTime}");
 
-            agent.Command = "select * from QuestionnaireResult where ProfileID = @profileId";
-            var tableResult = agent.Execute();
-            Trace.WriteLine($"Results from  table {tableResult.ExecutionTime}");
-        }
+        //    agent.Command = "select * from QuestionnaireResult where ProfileID = @profileId";
+        //    var tableResult = agent.Execute();
+        //    Trace.WriteLine($"Results from  table {tableResult.ExecutionTime}");
+        //}
 
-        [TestMethod()]
-        public void TestEmTest2()
-        {
-            var agent = new SqlLoadTask
-            {
-                ConnectionString = "Data Source=u7ksu7yxs0.database.windows.net;Database=SharedEpisodeTracker;Integrated Security=False;User ID=ETDev;Password=d3v3lupm!nt;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False",
-                Command = "select * from DataPointView where ProfileID = @profileId",
-                ReuseConnection = true
-            };
+        //[TestMethod()]
+        //public void TestEmTest2()
+        //{
+        //    var agent = new SqlLoadTask
+        //    {
+        //        ConnectionString = "Data Source=u7ksu7yxs0.database.windows.net;Database=SharedEpisodeTracker;Integrated Security=False;User ID=ETDev;Password=d3v3lupm!nt;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False",
+        //        Command = "select * from DataPointView where ProfileID = @profileId",
+        //        ReuseConnection = true
+        //    };
 
-            agent.CommandParameters.Add(new KeyValuePair<string, IValueGetter>("@profileId", new StringValueGetter("170FD672-7B0B-4A97-B0A7-6B2FADD148D4")));
+        //    agent.CommandParameters.Add(new KeyValuePair<string, IValueGetter>("@profileId", new StringValueGetter("170FD672-7B0B-4A97-B0A7-6B2FADD148D4")));
 
-            var viewResult = agent.Execute();
+        //    var viewResult = agent.Execute();
 
-            Trace.WriteLine($"2");
-            Trace.WriteLine($"Results from  view {viewResult.ExecutionTime}");
+        //    Trace.WriteLine($"2");
+        //    Trace.WriteLine($"Results from  view {viewResult.ExecutionTime}");
 
-            viewResult = agent.Execute();
+        //    viewResult = agent.Execute();
 
-            Trace.WriteLine($"Results from  view {viewResult.ExecutionTime}");
+        //    Trace.WriteLine($"Results from  view {viewResult.ExecutionTime}");
 
-            agent.Command = "select * from QuestionnaireResult where ProfileID = @profileId";
-            var tableResult = agent.Execute();
-            Trace.WriteLine($"Results from  table {tableResult.ExecutionTime}");
+        //    agent.Command = "select * from QuestionnaireResult where ProfileID = @profileId";
+        //    var tableResult = agent.Execute();
+        //    Trace.WriteLine($"Results from  table {tableResult.ExecutionTime}");
 
-            tableResult = agent.Execute();
-            Trace.WriteLine($"Results from  table {tableResult.ExecutionTime}");
-        }
+        //    tableResult = agent.Execute();
+        //    Trace.WriteLine($"Results from  table {tableResult.ExecutionTime}");
+        //}
 
-        [TestMethod()]
-        public void TestEmTest3()
-        {
-            var agent = new SqlLoadTask
-            {
-                ConnectionString = conStr,
-                Command = "select * from NativeDataPointView where ProfileID = @profileId",
-                ReuseConnection = true
-            };
+        //[TestMethod()]
+        //public void TestEmTest3()
+        //{
+        //    var agent = new SqlLoadTask
+        //    {
+        //        ConnectionString = conStr,
+        //        Command = "select * from NativeDataPointView where ProfileID = @profileId",
+        //        ReuseConnection = true
+        //    };
 
-            agent.CommandParameters.Add(new KeyValuePair<string, IValueGetter>("@profileId", new StringValueGetter("170FD672-7B0B-4A97-B0A7-6B2FADD148D4")));
+        //    agent.CommandParameters.Add(new KeyValuePair<string, IValueGetter>("@profileId", new StringValueGetter("170FD672-7B0B-4A97-B0A7-6B2FADD148D4")));
 
-            var viewResult = agent.Execute();
+        //    var viewResult = agent.Execute();
 
-            Trace.WriteLine($"3");
-            Trace.WriteLine($"Results from  view {viewResult.ExecutionTime}");
+        //    Trace.WriteLine($"3");
+        //    Trace.WriteLine($"Results from  view {viewResult.ExecutionTime}");
 
-            viewResult = agent.Execute();
-            Trace.WriteLine($"Results from  view {viewResult.ExecutionTime}");
+        //    viewResult = agent.Execute();
+        //    Trace.WriteLine($"Results from  view {viewResult.ExecutionTime}");
 
-            var commandBuilder = new StringBuilder("select * from QuestionnaireResult where ProfileID = @profileId ");
-            commandBuilder.AppendLine("and [QuestionnaireTypeID] in (SELECT [ID] FROM [dbo].[QuestionnaireType] where[IsComputed] = 0)");
-            agent.Command = commandBuilder.ToString();
+        //    var commandBuilder = new StringBuilder("select * from QuestionnaireResult where ProfileID = @profileId ");
+        //    commandBuilder.AppendLine("and [QuestionnaireTypeID] in (SELECT [ID] FROM [dbo].[QuestionnaireType] where[IsComputed] = 0)");
+        //    agent.Command = commandBuilder.ToString();
 
-            var tableResult = agent.Execute();
-            Trace.WriteLine($"Results from  table {tableResult.ExecutionTime}");
+        //    var tableResult = agent.Execute();
+        //    Trace.WriteLine($"Results from  table {tableResult.ExecutionTime}");
 
-            tableResult = agent.Execute();
-            Trace.WriteLine($"Results from  table {tableResult.ExecutionTime}");
-        }
+        //    tableResult = agent.Execute();
+        //    Trace.WriteLine($"Results from  table {tableResult.ExecutionTime}");
+        //}
 
-        [TestMethod()]
-        public void TestEmTest4()
-        {
-            var agent = new SqlLoadTask
-            {
-                ConnectionString = conStr,
-                Command = "select * from ComputedDataPointView where ProfileID = @profileId",
-                ReuseConnection = true
-            };
+        //[TestMethod()]
+        //public void TestEmTest4()
+        //{
+        //    var agent = new SqlLoadTask
+        //    {
+        //        ConnectionString = conStr,
+        //        Command = "select * from ComputedDataPointView where ProfileID = @profileId",
+        //        ReuseConnection = true
+        //    };
 
-            agent.CommandParameters.Add(new KeyValuePair<string, IValueGetter>("@profileId", new StringValueGetter("170FD672-7B0B-4A97-B0A7-6B2FADD148D4")));
+        //    agent.CommandParameters.Add(new KeyValuePair<string, IValueGetter>("@profileId", new StringValueGetter("170FD672-7B0B-4A97-B0A7-6B2FADD148D4")));
 
-            var viewResult = agent.Execute();
+        //    var viewResult = agent.Execute();
 
-            Trace.WriteLine($"4");
-            Trace.WriteLine($"Results from  view {viewResult.ExecutionTime}");
+        //    Trace.WriteLine($"4");
+        //    Trace.WriteLine($"Results from  view {viewResult.ExecutionTime}");
 
-            viewResult = agent.Execute();
-            Trace.WriteLine($"Results from  view {viewResult.ExecutionTime}");
+        //    viewResult = agent.Execute();
+        //    Trace.WriteLine($"Results from  view {viewResult.ExecutionTime}");
 
-            var commandBuilder = new StringBuilder("select * from QuestionnaireResult where ProfileID = @profileId ");
-            commandBuilder.AppendLine("and [QuestionnaireTypeID] in (SELECT [ID] FROM [dbo].[QuestionnaireType] where[IsComputed] = 1)");
-            agent.Command = commandBuilder.ToString();
+        //    var commandBuilder = new StringBuilder("select * from QuestionnaireResult where ProfileID = @profileId ");
+        //    commandBuilder.AppendLine("and [QuestionnaireTypeID] in (SELECT [ID] FROM [dbo].[QuestionnaireType] where[IsComputed] = 1)");
+        //    agent.Command = commandBuilder.ToString();
 
-            var tableResult = agent.Execute();
-            Trace.WriteLine($"Results from  table {tableResult.ExecutionTime}");
+        //    var tableResult = agent.Execute();
+        //    Trace.WriteLine($"Results from  table {tableResult.ExecutionTime}");
 
-            tableResult = agent.Execute();
-            Trace.WriteLine($"Results from  table {tableResult.ExecutionTime}");
-        }
+        //    tableResult = agent.Execute();
+        //    Trace.WriteLine($"Results from  table {tableResult.ExecutionTime}");
+        //}
 
-        [TestMethod()]
-        public void TestEmTest5()
-        {
-            using (var set = new SqlLoadSet { ConnectionString = conStr })
-            {
-                set.CommandParameters.Add(new KeyValuePair<string, IValueGetter>("@profileId", new StringValueGetter("170FD672-7B0B-4A97-B0A7-6B2FADD148D4")));
-                var cmd1 = new StringBuilder("select * from QuestionnaireResult where ProfileID = @profileId ");
-                var cmd2 = new StringBuilder(cmd1.ToString());
-                cmd2.AppendLine("and [QuestionnaireTypeID] in (SELECT [ID] FROM [dbo].[QuestionnaireType] where[IsComputed] = 0)");
-                var cmd3 = new StringBuilder(cmd1.ToString());
-                cmd3.AppendLine("and [QuestionnaireTypeID] in (SELECT [ID] FROM [dbo].[QuestionnaireType] where[IsComputed] = 1)");
+        //[TestMethod()]
+        //public void TestEmTest5()
+        //{
+        //    using (var set = new SqlLoadSet { ConnectionString = conStr })
+        //    {
+        //        set.CommandParameters.Add(new KeyValuePair<string, IValueGetter>("@profileId", new StringValueGetter("170FD672-7B0B-4A97-B0A7-6B2FADD148D4")));
+        //        var cmd1 = new StringBuilder("select * from QuestionnaireResult where ProfileID = @profileId ");
+        //        var cmd2 = new StringBuilder(cmd1.ToString());
+        //        cmd2.AppendLine("and [QuestionnaireTypeID] in (SELECT [ID] FROM [dbo].[QuestionnaireType] where[IsComputed] = 0)");
+        //        var cmd3 = new StringBuilder(cmd1.ToString());
+        //        cmd3.AppendLine("and [QuestionnaireTypeID] in (SELECT [ID] FROM [dbo].[QuestionnaireType] where[IsComputed] = 1)");
 
-                set.Tasks.Add(new SqlLoadTask { Command = cmd1.ToString() });
-                set.Tasks.Add(new SqlLoadTask { Command = cmd2.ToString() });
-                set.Tasks.Add(new SqlLoadTask { Command = cmd3.ToString() });
+        //        set.Tasks.Add(new SqlLoadTask { Command = cmd1.ToString() });
+        //        set.Tasks.Add(new SqlLoadTask { Command = cmd2.ToString() });
+        //        set.Tasks.Add(new SqlLoadTask { Command = cmd3.ToString() });
 
-                var result = set.Execute();
-            }
-        }
+        //        var result = set.Execute();
+        //    }
+        //}
 
-        [TestMethod()]
-        public void TestEmTest6()
-        {
-            using (var set = new SqlLoadSet())
-            {
-                set.CommandParameters.Add(new KeyValuePair<string, IValueGetter>("@profileId", new StringValueGetter("170FD672-7B0B-4A97-B0A7-6B2FADD148D4")));
+        //[TestMethod()]
+        //public void TestEmTest6()
+        //{
+        //    using (var set = new SqlLoadSet())
+        //    {
+        //        set.CommandParameters.Add(new KeyValuePair<string, IValueGetter>("@profileId", new StringValueGetter("170FD672-7B0B-4A97-B0A7-6B2FADD148D4")));
 
-                var cmd1 = new StringBuilder("select * from QuestionnaireResult where ProfileID = @profileId ");
-                var cmd2 = new StringBuilder(cmd1.ToString());
-                cmd2.AppendLine("and [QuestionnaireTypeID] in (SELECT [ID] FROM [dbo].[QuestionnaireType] where[IsComputed] = 0)");
-                var cmd3 = new StringBuilder(cmd1.ToString());
-                cmd3.AppendLine("and [QuestionnaireTypeID] in (SELECT [ID] FROM [dbo].[QuestionnaireType] where[IsComputed] = 1)");
+        //        var cmd1 = new StringBuilder("select * from QuestionnaireResult where ProfileID = @profileId ");
+        //        var cmd2 = new StringBuilder(cmd1.ToString());
+        //        cmd2.AppendLine("and [QuestionnaireTypeID] in (SELECT [ID] FROM [dbo].[QuestionnaireType] where[IsComputed] = 0)");
+        //        var cmd3 = new StringBuilder(cmd1.ToString());
+        //        cmd3.AppendLine("and [QuestionnaireTypeID] in (SELECT [ID] FROM [dbo].[QuestionnaireType] where[IsComputed] = 1)");
 
-                set.Tasks.Add(new SqlLoadTask { Command = cmd1.ToString(), ConnectionString = conStr, ReuseConnection = true });
-                set.Tasks.Add(new SqlLoadTask { Command = cmd2.ToString(), ConnectionString = conStr, ReuseConnection = true });
-                set.Tasks.Add(new SqlLoadTask { Command = cmd3.ToString(), ConnectionString = conStr, ReuseConnection = true });
+        //        set.Tasks.Add(new SqlLoadTask { Command = cmd1.ToString(), ConnectionString = conStr, ReuseConnection = true });
+        //        set.Tasks.Add(new SqlLoadTask { Command = cmd2.ToString(), ConnectionString = conStr, ReuseConnection = true });
+        //        set.Tasks.Add(new SqlLoadTask { Command = cmd3.ToString(), ConnectionString = conStr, ReuseConnection = true });
 
-                var result = set.Execute();
-            }
-        }
+        //        var result = set.Execute();
+        //    }
+        //}
 
-        [TestMethod()]
-        public void TestEmTest7()
-        {
-            var set = new SqlLoadSet { ConnectionString = conStr };
-            set.CommandParameters.Add(new KeyValuePair<string, IValueGetter>("@profileId", new StringValueGetter("170FD672-7B0B-4A97-B0A7-6B2FADD148D4")));
+        //[TestMethod()]
+        //public void TestEmTest7()
+        //{
+        //    var set = new SqlLoadSet { ConnectionString = conStr };
+        //    set.CommandParameters.Add(new KeyValuePair<string, IValueGetter>("@profileId", new StringValueGetter("170FD672-7B0B-4A97-B0A7-6B2FADD148D4")));
 
-            set.Tasks.Add(new SqlLoadTask { Command = "select * from DataPointView where ProfileID = @profileId " });
-            set.Tasks.Add(new SqlLoadTask { Command = "select * from NativeDataPointView where ProfileID = @profileId" });
-            set.Tasks.Add(new SqlLoadTask { Command = "select * from ComputedDataPointView where ProfileID = @profileId" });
+        //    set.Tasks.Add(new SqlLoadTask { Command = "select * from DataPointView where ProfileID = @profileId " });
+        //    set.Tasks.Add(new SqlLoadTask { Command = "select * from NativeDataPointView where ProfileID = @profileId" });
+        //    set.Tasks.Add(new SqlLoadTask { Command = "select * from ComputedDataPointView where ProfileID = @profileId" });
 
-            var result = set.Execute();
-        }
+        //    var result = set.Execute();
+        //}
 
-        [TestMethod()]
-        public void TestEmTest8()
-        {
-            using (var set = new SqlLoadSet())
-            {
-                set.CommandParameters.Add(new KeyValuePair<string, IValueGetter>("@profileId", new StringValueGetter("170FD672-7B0B-4A97-B0A7-6B2FADD148D4")));
+        //[TestMethod()]
+        //public void TestEmTest8()
+        //{
+        //    using (var set = new SqlLoadSet())
+        //    {
+        //        set.CommandParameters.Add(new KeyValuePair<string, IValueGetter>("@profileId", new StringValueGetter("170FD672-7B0B-4A97-B0A7-6B2FADD148D4")));
 
-                set.Tasks.Add(new SqlLoadTask { Command = "select * from DataPointView where ProfileID = @profileId", ConnectionString = conStr });
-                set.Tasks.Add(new SqlLoadTask { Command = "select * from NativeDataPointView where ProfileID = @profileId", ConnectionString = conStr });
-                set.Tasks.Add(new SqlLoadTask { Command = "select * from ComputedDataPointView where ProfileID = @profileId", ConnectionString = conStr });
+        //        set.Tasks.Add(new SqlLoadTask { Command = "select * from DataPointView where ProfileID = @profileId", ConnectionString = conStr });
+        //        set.Tasks.Add(new SqlLoadTask { Command = "select * from NativeDataPointView where ProfileID = @profileId", ConnectionString = conStr });
+        //        set.Tasks.Add(new SqlLoadTask { Command = "select * from ComputedDataPointView where ProfileID = @profileId", ConnectionString = conStr });
 
-                var result = set.Execute();
-            }
-        }
+        //        var result = set.Execute();
+        //    }
+        //}
     }
 }
--- LoadTestEmTests/LoadSets/LoadSetTests.cs
+++ LoadTestEmTests/LoadSets/LoadSetTests.cs
@@ -13,49 +13,49 @@ namespace LoadTestEm.LoadSets.Tests
     [TestClass()]
     public class LoadSetTests
     {
-        [TestMethod()]
-        public void ExecuteAsyncTest()
-        {
-            var loadSet = new LoadSet();
+        //[TestMethod()]
+        //public void ExecuteAsyncTest()
+        //{
+        //    var loadSet = new LoadSet();
 
-            var conStr = "Data Source=(local);Initial Catalog=LoadTestEm;Integrated Security=SSPI";
-            var cmd = "insert into Names (Name) values (@param1)";
+        //    var conStr = "Data Source=(local);Initial Catalog=LoadTestEm;Integrated Security=SSPI";
+        //    var cmd = "insert into Names (Name) values (@param1)";
 
-            var loadTask1 = new SqlLoadTask
-            {
-                ConnectionString = conStr,
-                Command = cmd
-            };
+        //    var loadTask1 = new SqlLoadTask
+        //    {
+        //        ConnectionString = conStr,
+        //        Command = cmd
+        //    };
 
-            loadTask1.CommandParameters.Add(new KeyValuePair<string, IValueGetter>("@param1", new RandomStringValueGetter(5, "One-")));
-            loadSet.Tasks.Add(loadTask1);
+        //    loadTask1.CommandParameters.Add(new KeyValuePair<string, IValueGetter>("@param1", new RandomStringValueGetter(5, "One-")));
+        //    loadSet.Tasks.Add(loadTask1);
 
-            cmd = "select * from Names where name like 'One-%'";
-            var loadTask2 = new SqlLoadTask
-            {
-                ConnectionString = conStr,
-                Command = cmd
-            };
-            loadSet.Tasks.Add(loadTask2);
+        //    cmd = "select * from Names where name like 'One-%'";
+        //    var loadTask2 = new SqlLoadTask
+        //    {
+        //        ConnectionString = conStr,
+        //        Command = cmd
+        //    };
+        //    loadSet.Tasks.Add(loadTask2);
 
-            cmd = "Update Names set Name = @newName where name like 'One-%'"; 
-            var loadTask3 = new SqlLoadTask
-            {
-                ConnectionString = conStr,
-                Command = cmd
-            };
-            loadTask3.CommandParameters.Add(new KeyValuePair<string, IValueGetter>("@newName", new RandomStringValueGetter(5, "Three-")));            
-            loadSet.Tasks.Add(loadTask3);
+        //    cmd = "Update Names set Name = @newName where name like 'One-%'"; 
+        //    var loadTask3 = new SqlLoadTask
+        //    {
+        //        ConnectionString = conStr,
+        //        Command = cmd
+        //    };
+        //    loadTask3.CommandParameters.Add(new KeyValuePair<string, IValueGetter>("@newName", new RandomStringValueGetter(5, "Three-")));            
+        //    loadSet.Tasks.Add(loadTask3);
 
-            cmd = "delete from Names where name like 'Three-%'";
-            var loadTask4 = new SqlLoadTask
-            {
-                ConnectionString = conStr,
-                Command = cmd
-            };
-            loadSet.Tasks.Add(loadTask4);
+        //    cmd = "delete from Names where name like 'Three-%'";
+        //    var loadTask4 = new SqlLoadTask
+        //    {
+        //        ConnectionString = conStr,
+        //        Command = cmd
+        //    };
+        //    loadSet.Tasks.Add(loadTask4);
 
-            var total = Task.Run(() => loadSet.ExecuteAsync()).Result;
-        }
+        //    var total = Task.Run(() => loadSet.ExecuteAsync()).Result;
+        //}
     }
 }
